b215310afdd69e0c410b87efa49ca6d92647b8c7
John Prince <jtprince@gmail.com>
2008-09-02 17:25:34 -0600

    added a way to calculate the pi0

---
+++ lib/pi_zero.rb
@@ -0,0 +1,83 @@
+require 'rsruby'
+
+module PiZero
+  # takes a sorted array of p-values (floats between 0 and 1 inclusive)
+  # returns [thresholds_ar, instantaneous pi_0 calculations_ar]
+  # evenly incremented values will be used by default:
+  # :start=>0.0, :stop=>1.0, :step=>0.01
+  def self.pi_zero_hats(sorted_pvals, args={})
+    defaults = {:start => 0.0, :stop=>1.0, :step=>0.01 }
+    margs = defaults.merge( args )
+    (start, stop, step) = margs.values_at(:start, :stop, :step)
+
+    # From Storey et al. PNAS 2003:
+    lambdas = []                 # lambda
+    pi_zeros = []                # pi_0
+    total = sorted_pvals.size  # m 
+
+    # totally retarded implementation with correct logic:
+    start.step(stop, step) do |lam|
+      lambdas << lam
+      (greater, less) = sorted_pvals.partition {|pval| pval > lam }
+      pi_zeros.push( greater.size.to_f / ( total * (1.0 - lam) ) )
+    end
+    [lambdas, pi_zeros]
+  end
+
+  # expecting x and y to make a scatter plot descending to a plateau on the
+  # right side (which is assumed to be of increasing noise as it goes to the
+  # right)
+  # returns the height of the plateau
+  #
+  # *
+  #   *
+  #     *
+  #       **
+  #          ** ***         *    *
+  #                    ***** **** ***
+  def self.plateau(x, y)
+=begin
+    require 'gsl'
+    x_deltas = (0...(x.size-1)).to_a.map do |i|
+      x[i+1] - x[i]
+    end
+    y_deltas = (0...(y.size-1)).to_a.map do |i|
+      y[i+1] - y[i]
+    end
+    new_xs = x.dup
+    new_ys = y.dup
+    x_deltas.reverse.each do |delt|
+      new_xs.push( new_xs.last + delt )
+    end
+    
+    y_cnt = y.size
+    y_deltas.reverse.each do |delt|
+      y_cnt -= 1
+      new_ys.push( y[y_cnt] - delt )
+    end
+
+    x_vec = GSL::Vector.alloc(new_xs)
+    y_vec = GSL::Vector.alloc(new_ys)
+    coef, cov, chisq, status = GSL::Poly.fit(x_vec,y_vec, 3)
+    coef.eval(x.last)
+    #x2 = GSL::Vector::linspace(0,2.4,20)
+    #graph([x_vec,y_vec], [x2, coef.eval(x2)], "-C -g 3 -S 4")
+=end
+
+    r = RSRuby.instance
+    answ = r.smooth_spline(x,y, :df => 3)
+    ## to plot it!
+    #r.plot(x,y)
+    #r.lines(answ['x'], answ['y'])
+    #sleep(8)
+
+    answ['y'].last
+  end
+
+  # returns a conservative (but close) estimate of pi_0 given sorted p-values
+  # following Storey et al. 2003, PNAS.
+  def self.pi_zero(sorted_pvals)
+    plateau( *(pi_zero_hats(sorted_pvals)) )
+  end
+
+end
---
+++ specs/pi_zero_spec.rb
@@ -0,0 +1,34 @@
+require File.expand_path( File.dirname(__FILE__) + '/spec_helper' )
+require 'pi_zero'
+
+describe PiZero do
+  before(:all) do
+                     
+    @sorted_pvals = [0.0, 0.1, 0.223, 0.24, 0.55, 0.68, 0.68, 0.90, 0.98, 1.0]
+  end
+
+  it 'calculates instantaneous pi_0 hats' do
+    answ = PiZero.pi_zero_hats(@sorted_pvals, :step => 0.1)
+    exp_lambdas =       [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    passing_threshold = [9,   8,   8,   6,   6,   6,   5,   3,   3,   2,   0]
+    expected = passing_threshold.zip(exp_lambdas).map {|v,l| v.to_f / (10.0 * (1.0 - l)) }
+    (answ_lams, answ_pis) = answ
+    answ_lams.zip(exp_lambdas) {|a,e| a.should be_close(e, 0.0000000001) }
+    (answ_pis.pop.nan?).should be_true
+    (expected.pop.nan?).should be_true
+    answ_pis.zip(expected) {|a,e| a.should be_close(e, 0.0000000001) }
+  end
+
+  it 'can find a plateau region' do
+    x = [0.0, 0.01, 0.012, 0.13, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2]
+    y = [1.0, 0.95, 0.92, 0.8, 0.7, 0.6, 0.55, 0.58, 0.62, 0.53, 0.54, 0.59, 0.4, 0.72]
+    z = PiZero.plateau(x,y)
+    z.should be_close(0.57, 0.05)
+    #require 'rsruby'
+    #r = RSRuby.instance
+    #r.plot(x,y)
+    #sleep(8)
+  end
+
+end
+
