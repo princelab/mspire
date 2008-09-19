a746a1d1a89d9ec35cc19c0751cca84fe8052262
John Prince <jtprince@gmail.com>
2008-09-18 22:36:14 -0600

    progress

---
+++ lib/core_extensions.rb
@@ -0,0 +1,16 @@
+
+class Float
+  # 3 following methods from http://www.hans-eric.com/code-samples/ruby-floating-point-round-off/
+  def round_to(x)
+    (self * 10**x).round.to_f / 10**x
+  end
+
+  def ceil_to(x)
+    (self * 10**x).ceil.to_f / 10**x
+  end
+
+  def floor_to(x)
+    (self * 10**x).floor.to_f / 10**x
+  end
+end
+
