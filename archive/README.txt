7cdb2408bbeb1671758a0fe4f7a5e9e06dd2df8f
John Prince <jtprince@gmail.com>
2009-05-11 17:40:13 -0600

    $1

--- archive/README.txt
+++
@@ -1,1 +0,0 @@
-t2x is the ReAdw t2x v2.5 compiled on Redhat 7.3 (should work with compatible gcc libraries)
--- depracated/specs-bin-precision_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe 'precision.rb' do
-  before(:all) do
-    @progname = 'precision.rb'
-  end
-
-  it_should_behave_like "a cmdline program"
-end
