db96e5096a5abbdf0dd4b61b0b9076f739a567e2
Ryan Taylor <ryanmt@byu.net>
2012-02-15 14:51:04 -0700

    class variables?

--- lib/ms/isotope/isotope.rb
+++ lib/ms/isotope/isotope.rb
@@ -1,8 +1,16 @@
 module MS
   module Isotope
-    Isotopes = nil
-    IsotopicAbundanceAndMassByElement = nil
-    Isotope_Elements = nil
+    class << self
+      def IsotopicAbundanceAndMassByElement
+        @@isotopic_abundance_and_mass_by_element
+      end
+      def Isotopes
+        @@isotopes
+      end
+      def IsotopesByElements
+        @@isotopes_by_elements
+      end
+    end # class << self
     Isotope = Struct.new(:atomic_number, :element, :mass_number, :atomic_mass, :relative_abundance, :average_mass, :mono) do 
       def initialize(*args)
         super(*args, false)
@@ -18,28 +26,24 @@ module MS
           body = Mechanize.new.get('http://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl?ele=&all=all&ascii=ascii2&isotype=some').body.split("\n")
           body.delete_if {|l| l[/^(<|\/)/]}
           body.shift(22)
-          isotopes = body.each_slice(8).map do |lines|
+          @@isotopes = body.each_slice(8).map do |lines|
             arr = (1..4).to_a.map {|i| match lines[i] }
             rel, avg = match(lines[5]), match(lines[6])
             next if rel.nil?
             rel.size > 0 ? Isotope.new(*arr, rel, avg) : nil
           end.compact!
-          const_set("Isotopes", isotopes)
         end #parse_NIST_site
 
         def group_by_element
-          elements = Isotopes.map(&:element)
-          isotope_Elements = {}
+          elements = @@isotopes.map(&:element)
+          @@isotopes_by_elements = {}
           elements.each do |e|
-            isotope_Elements[e] = []
+            @@isotopes_by_elements[e] = []
           end
-          Isotopes.each do |iso|
-            isotope_Elements[iso.element] << iso
+          @@isotopes.each do |iso|
+            @@isotopes_by_elements[iso.element] << iso
           end
-          const_set("Isotope_Elements", isotope_Elements)
-          arr =  Hash[Isotope_Elements.map {|k,val| [k, val.map {|v| [v.relative_abundance, v.atomic_mass, v.mass_number] }   ]}]  
-          const_set("IsotopicAbundanceAndMassByElement", arr )
-          p IsotopicAbundanceAndMassByElement
+          @@isotopic_abundance_and_mass_by_element =  Hash[@@isotopes_by_elements.map {|k,val| [k, val.map {|v| [v.relative_abundance, v.atomic_mass, v.mass_number] }   ]}]  
         end #group_by_element
         private
         def match(string)
@@ -59,4 +63,6 @@ end # module MS
 MS::Isotope::Updater.parse_NIST_site
 MS::Isotope::Updater.group_by_element
 
-p MS::Isotope::IsotopicAbundanceAndMassByElement
+require 'pry'
+binding.pry
+p MS::Isotope.IsotopicAbundanceAndMassByElement
