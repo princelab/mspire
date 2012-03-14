a5d9a6cb976efa79bbebbfdf4ea1090e547c0e62
John Prince <jtprince@gmail.com>
2012-03-14 17:36:31 -0600

    adding mass and from_aaseq

--- lib/ms/molecular_formula.rb
+++ lib/ms/molecular_formula.rb
@@ -1,13 +1,19 @@
 require 'ms/isotope'
+require 'ms/isotope/aa'
+require 'ms/mass'
 
 module MS
   class MolecularFormula < Hash
 
+    # integer desribing the charge state
+    # mass calculations will add/remove electron mass from this
+    attr_accessor :charge
     # takes a string or a hash:
     #
     #     "H22C12N1O3S2BeLi2"                    # <= order doesn't matter
     #     {h: 22, c: 12, n: 1, o: 3, s: 2}  # case and string/sym doesn't matter
-    def initialize(arg)
+    def initialize(arg, charge=0)
+      @charge = charge
       if arg.is_a?(String)
         arg.scan(/([A-Z][a-z]?)(\d*)/).each do |k,v| 
           self[k.downcase.to_sym] = (v == '' ? 1 : v.to_i)
@@ -15,6 +21,21 @@ module MS
       else
         self.merge!(arg)
       end
+    end
+
+    def self.from_aaseq(aaseq)
+      hash = aaseq.each_char.inject({}) do |hash,aa| 
+        hash.merge(MS::Isotope::AA::ATOM_COUNTS[aa]) {|h,o,n| (o ? o : 0) +n }
+      end
+      hash[:h] += 2
+      hash[:o] += 1
+      self.new(hash)
+    end
+
+    # gives the monoisotopic mass adjusted by the current charge
+    def mass
+      mss = inject(0.0) {|sum,(el,cnt)| sum + (MS::Mass::MONO[el]*cnt) }
+      mss - (MS::Mass::ELECTRON * charge)
     end
 
   end
