6f6c5f52c737e9c36ed9f3131537251fd47e1473
John Prince <jtprince@gmail.com>
2012-03-15 23:48:19 -0600

    fixed spec that needed fixing (not a bug)

--- spec/mspire/isotope/distribution_spec.rb
+++ spec/mspire/isotope/distribution_spec.rb
@@ -4,7 +4,7 @@ require 'mspire/isotope/distribution'
 
 describe 'Mspire::Isotope::Distribution class methods' do
   before do 
-    @data = [1.0, 0.08919230588715289, 0.017894161377222082, 0.0013573997600723623, 0.0001398330738144092]
+    @data = [1.0, 0.08919230588715311, 0.017894161377222138, 0.0013573997600723345, 0.0001398330738144181]
   end
 
   it 'can calculate isotope distributions' do
@@ -15,6 +15,6 @@ describe 'Mspire::Isotope::Distribution class methods' do
   it 'can calculate isotope distribution spectrum' do
     spec = Mspire::Isotope::Distribution.spectrum('C8O7', :max)
     spec.mzs.should == [207.96440233692, 208.97306725252, 209.98173216812, 210.99039708372, 211.99906199932002]
-    spec.intensities.should == [1.0, 0.08919230588715289, 0.017894161377222082, 0.0013573997600723623, 0.0001398330738144092]
+    spec.intensities.should == [1.0, 0.08919230588715311, 0.017894161377222138, 0.0013573997600723345, 0.0001398330738144181]
   end
 end
