cbe974c759ddad6d0ad767a4223cb948efa0dbf4
John Prince <jtprince@gmail.com>
2012-07-09 22:51:51 -0600

    added and tested molecular formula arithmetic

--- lib/mspire/molecular_formula.rb
+++ lib/mspire/molecular_formula.rb
@@ -4,46 +4,125 @@ require 'mspire/mass'
 module Mspire
   class MolecularFormula < Hash
 
+    class << self
+      def from_aaseq(aaseq)
+        hash = aaseq.each_char.inject({}) do |hash,aa| 
+          hash.merge(Mspire::Isotope::AA::FORMULAS[aa]) {|h,o,n| (o ? o : 0) +n }
+        end
+        hash[:h] += 2
+        hash[:o] += 1
+        self.new(hash)
+      end
+
+      # takes a string, properly capitalized with the formula.  The elements
+      # may be in any order.
+      #
+      #     "H22C12N1O3S2BeLi2"                    # <= order doesn't matter
+      def from_string(mol_form_str, charge=0)
+        mf = self.new({}, charge)
+        mol_form_str.scan(/([A-Z][a-z]?)(\d*)/).each do |k,v| 
+          mf[k.downcase.to_sym] = (v == '' ? 1 : v.to_i)
+        end
+        mf
+      end
+
+      alias_method :[], :from_string
+    end
+
     # integer desribing the charge state
     # mass calculations will add/remove electron mass from this
     attr_accessor :charge
-    # takes a string or a hash:
-    #
-    #     "H22C12N1O3S2BeLi2"                    # <= order doesn't matter
+
+    # Takes a hash and an optional Integer expressing the charge
     #     {h: 22, c: 12, n: 1, o: 3, s: 2}  # case and string/sym doesn't matter
-    def initialize(arg, charge=0)
+    def initialize(hash={}, charge=0)
       @charge = charge
-      if arg.is_a?(String)
-        arg.scan(/([A-Z][a-z]?)(\d*)/).each do |k,v| 
-          self[k.downcase.to_sym] = (v == '' ? 1 : v.to_i)
-        end
-      else
-        self.merge!(arg)
-      end
+      self.merge!(hash)
     end
 
     # returns a new formula object where all the atoms have been added up
     def +(*others)
-      new_form = self.dup
-      others.each do |form|
-        new_form.merge!(form) {|key, oldval, newval| new_form[key] = oldval+newval }
-      end
-      new_form
+      self.dup.add!(*others)
     end
 
-    def self.from_aaseq(aaseq)
-      hash = aaseq.each_char.inject({}) do |hash,aa| 
-        hash.merge(Mspire::Isotope::AA::FORMULAS[aa]) {|h,o,n| (o ? o : 0) +n }
+    # returns self
+    def add!(*others)
+      others.each do |other|
+        self.merge!(other) {|key, oldval, newval| self[key] = oldval + newval }
+        self.charge += other.charge
       end
-      hash[:h] += 2
-      hash[:o] += 1
-      self.new(hash)
+      self
     end
 
-    # gives the monoisotopic mass adjusted by the current charge
-    def mass
+    # returns a new formula object where all the formulas have been subtracted
+    # from the caller
+    def -(*others)
+      self.dup.sub!(*others)
+    end
+
+    def sub!(*others)
+      others.each do |other|
+        oth = other.dup
+        self.each do |k,v|
+          if oth.key?(k)
+            self[k] -= oth.delete(k)
+          end
+        end
+        oth.each do |k,v|
+          self[k] = -v
+        end
+        self.charge -= other.charge
+      end
+      self
+    end
+
+    def *(int)
+      self.dup.mul!(int)
+    end
+
+    def mul!(int, also_do_charge=true)
+      raise ArgumentError, "must be an integer" unless int.is_a?(Integer)
+      self.each do |k,v|
+        self[k] = v * int
+      end
+      self.charge *= int if also_do_charge
+      self
+    end
+
+    def /(int)
+      self.dup.div!(int)
+    end
+
+    def div!(int, also_do_charge=true)
+      raise ArgumentError, "must be an integer" unless int.is_a?(Integer)
+      self.each do |k,v|
+        quotient, modulus = v.divmod(int)
+        raise ArgumentError "all numbers must be divisible by int" unless modulus == 0
+        self[k] = quotient
+      end
+      if also_do_charge
+        quotient, modulus = self.charge.divmod(int) 
+        raise ArgumentError "charge must be divisible by int" unless modulus == 0
+        self.charge = quotient
+      end
+      self
+    end
+
+    # gives the monoisotopic mass adjusted by the current charge (i.e.,
+    # adds/subtracts electron masses for the charges)
+    def mass(consider_electron_masses = true)
       mss = inject(0.0) {|sum,(el,cnt)| sum + (Mspire::Mass::MONO[el]*cnt) }
-      mss - (Mspire::Mass::ELECTRON * charge)
+      mss -= (Mspire::Mass::ELECTRON * charge) if consider_electron_mass
+      mss
+    end
+
+    # returns nil if the charge == 0
+    def mz(consider_electron_masses = true)
+      if charge == 0
+        nil
+      else
+        mass(consider_electron_masses) / charge
+      end
     end
 
     def to_s(alphabetize=true)
--- spec/mspire/molecular_formula_spec.rb
+++ spec/mspire/molecular_formula_spec.rb
@@ -2,52 +2,109 @@ require 'spec_helper'
 
 require 'mspire/molecular_formula'
 
+MF = Mspire::MolecularFormula
 describe Mspire::MolecularFormula do
 
-  it 'can be initialized with a String or Hash' do
-    data = {h: 22, c: 12, n: 1, o: 3, s: 2}
-    mf = Mspire::MolecularFormula.new "H22BeC12N1O3S2Li2"
-    mf.to_hash.should == {:h=>22, :be=>1, :c=>12, :n=>1, :o=>3, :s=>2, :li=>2}
-    mf = Mspire::MolecularFormula.new(data)
-    mf.to_hash.should == data
-  end
+  describe 'initialization' do
 
-  it 'can be initialized with charge, too' do
-    mf = Mspire::MolecularFormula.new "H22BeC12N1O3S2Li2", 2
-    mf.to_hash.should == {:h=>22, :be=>1, :c=>12, :n=>1, :o=>3, :s=>2, :li=>2}
-    mf.charge.should == 2
-  end
-
-  it 'expects properly capitalized abbreviations' do
-    Mspire::MolecularFormula.new('Ni7Se3').to_hash.should == {:ni=>7, :se=>3}
-    # there is no such thing as the E element, so this is going to get the
-    # user in trouble.  However, this is the proper interpretation of the
-    # formula.
-    Mspire::MolecularFormula.new('Ni7SE3').to_hash.should == {:ni=>7, :s=>1, :e=>3}
-  end
-
-  describe 'an object' do
-    
-    subject {
-      data = {h: 22, c: 12, n: 1, o: 3, s: 2, be: 1}
-      Mspire::MolecularFormula.new(data)
-    }
-
-    it 'the string output is a standard molecular formula' do
-      subject.to_s.should == "BeC12H22NO3S2"
+    it 'is initialized with Hash' do
+      data = {h: 22, c: 12, n: 1, o: 3, s: 2}
+      mf = Mspire::MolecularFormula.new(data)
+      mf.to_hash.should == {:h=>22, :c=>12, :n=>1, :o=>3, :s=>2}
+      mf.to_hash.should == data
     end
 
-    it 'can be converted to a hash' do
-      subject.to_hash.should == {h: 22, c: 12, n: 1, o: 3, s: 2, be: 1}
+    it 'can be initialized with charge, too' do
+      mf = Mspire::MolecularFormula["H22BeC12N1O3S2Li2", 2]
+      mf.to_hash.should == {:h=>22, :be=>1, :c=>12, :n=>1, :o=>3, :s=>2, :li=>2}
+      mf.charge.should == 2
     end
 
-    it 'is only equal if the charge is equal' do
-      another = subject.dup
-      another.should == subject
-      another.charge = 2
-      another.should_not == subject
+    it 'from_string or ::[] to make from a capitalized string formula' do
+      Mspire::MolecularFormula.from_string("H22BeC12N1O3S2Li2").to_hash.should == {:h=>22, :be=>1, :c=>12, :n=>1, :o=>3, :s=>2, :li=>2}
+
+      mf = Mspire::MolecularFormula['Ni7Se3', 1]
+      mf.charge.should == 1
+      mf.to_hash.should == {:ni=>7, :se=>3}
+
+      # there is no such thing as the E element, so this is going to get the
+      # user in trouble.  However, this is the proper interpretation of the
+      # formula.
+      Mspire::MolecularFormula['Ni7SE3'].to_hash.should == {:ni=>7, :s=>1, :e=>3}
+    end
+
+    describe 'conversion' do
+
+      subject {
+        data = {h: 22, c: 12, n: 1, o: 3, s: 2, be: 1}
+        Mspire::MolecularFormula.new(data)
+      }
+
+      it 'the string output is a standard molecular formula' do
+        subject.to_s.should == "BeC12H22NO3S2"
+      end
+
+      it 'can be converted to a hash' do
+        subject.to_hash.should == {h: 22, c: 12, n: 1, o: 3, s: 2, be: 1}
+      end
+    end
+
+    describe 'equality' do
+      subject {
+        data = {h: 22, c: 12, n: 1, o: 3, s: 2, be: 1}
+        Mspire::MolecularFormula.new(data)
+      }
+      it 'is only equal if the charge is equal' do
+        another = subject.dup
+        another.should == subject
+        another.charge = 2
+        another.should_not == subject
+      end
+    end
+
+    describe 'arithmetic' do
+      subject {
+        data = {h: 22, c: 12, n: 1, o: 3, s: 2, be: 1}
+        Mspire::MolecularFormula.new(data, 2)
+      }
+      it 'can do non-destructive arithmetic' do
+        orig = subject.dup
+        reply = subject + MF["H2C3P2", 2]
+        reply.to_hash.should == {h: 24, c: 15, n: 1, o: 3, s: 2, be: 1, p: 2}
+        reply.charge.should == 4
+        subject.should == orig
+
+        reply = subject - MF["H2C3P2", 2]
+        reply.to_hash.should == {h: 20, c: 9, n: 1, o: 3, s: 2, be: 1, p: -2}
+        reply.charge.should == 0
+        subject.should == orig
+
+        by2 = subject * 2
+        by2.to_hash.should == {h: 44, c: 24, n: 2, o: 6, s: 4, be: 2}
+        by2.charge.should == 4
+        subject.should == orig
+
+        reply = by2 / 2
+        reply.to_hash.should == {h: 22, c: 12, n: 1, o: 3, s: 2, be: 1}
+        reply.charge.should == 2
+        subject.should == orig
+      end
+
+      it 'can do destructive arithmetic' do
+        orig = subject.dup
+        subject.sub!(MF["H2C3"]).to_hash.should == {h: 20, c: 9, n: 1, o: 3, s: 2, be: 1}
+        subject.should_not == orig
+        subject.add!(MF["H2C3"]).to_hash.should == {h: 22, c: 12, n: 1, o: 3, s: 2, be: 1}
+        subject.should == orig
+
+        by2 = subject.mul!(2)
+        subject.should_not == orig
+        by2.to_hash.should == {h: 44, c: 24, n: 2, o: 6, s: 4, be: 2}
+        by2.div!(2).to_hash.should == {h: 22, c: 12, n: 1, o: 3, s: 2, be: 1}
+        by2.to_hash.should == orig
+      end
+
     end
 
   end
-
 end
