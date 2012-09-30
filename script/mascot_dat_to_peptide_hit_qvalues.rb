ebd9c333de1a622ccce69c31775a413b5fea2940
John Prince <jtprince@gmail.com>
2012-09-29 23:58:54 -0600

    working on reading mascot dat for script

--- script/mascot_dat_to_peptide_hit_qvalues.rb
+++ script/mascot_dat_to_peptide_hit_qvalues.rb
@@ -2,25 +2,46 @@
 
 require 'trollop'
 require 'set'
-require 'mascot/dat'
-
-def read_mascot_dat_hits(dat_file)
-  dat = Mascot::DAT.open(dat_file)
-  dat.peptides.each do |pep|
-    p pep.score
-  end
-  dat.close
-end
+require 'mspire/ident/peptide_hit/qvalue'
+require 'mspire/error_rate/qvalue'
 
 begin
   require 'mascot/dat'
 rescue LoadError
   puts "You need the mascot-dat gem for this to work!"
+  puts "AND IT MUST BE THE PRINCELAB GITHUB FORK until changes get incorporated upstream!"
   puts ">     gem install mascot-dat"
   raise LoadError
 end
-require 'mspire/ident/peptide_hit/qvalue'
-require 'mspire/ident/pepxml'
+
+# target-decoy bundle
+TDB = Struct.new(:target, :decoy)
+
+PSM = Struct.new(:aaseq, :charge, :score, :qvalue) 
+
+# turns 1+ into 1
+def charge_string_to_charge(st)
+  md = st.match(/(\d)([\+\-])/)
+  i = md[1].to_i 
+  i *= -1 if (md[2] == '-')
+  i
+end
+
+def read_mascot_dat_hits(dat_file)
+  dat = Mascot::DAT.open(dat_file)
+  data = [:peptides, :decoy_peptides].each do |mthd|
+    psms = []
+    dat.send(mthd).each do |psm|
+      next unless psm.query
+      query = dat.query(psm.query)
+      charge = charge_string_to_charge(query.charge)
+      psms << PSM.new(psm.pep, charge, psm.score) if psm.score
+    end
+  end
+  dat.close
+  TDB.new(*data)
+end
+
 
 def putsv(*args)
   puts(*args) if $VERBOSE
@@ -52,33 +73,30 @@ $VERBOSE = opt.delete(:verbose)
 
 files = ARGV.to_a
 
-# groups_of_search_hits is just a list of alternating target and decoy hits
-groups_of_search_hits = []
-files.map do |file|
-  putsv "reading search hits from dat: #{file}"
-  (target, decoy) = read_mascot_dat_hits(file)
-  groups_of_search_hits << target << decoy
+bundles = files.map do |file|
+  # assumes the file has both target and decoy hits
+  read_mascot_dat_hits(file)
 end
 
 to_run = {}
 if opt[:combine]
   putsv "combining all target hits together and all decoy hits together"
   all_target = [] ; all_decoy = []
-  groups_of_search_hits.each_slice(2) do |target_hits,decoy_hits| 
-    all_target.push(*target_hits) ; all_decoy.push(*decoy_hits)
+  bundles.each do |bundle| 
+    all_target.push(*bundle.target) 
+    all_decoy.push(*bundle.decoy)
   end
-  to_run[combine_base + EXT] = [all_target, all_decoy]
+  bundle = TDB.new(all_target, all_decoy)
+  to_run[combine_base + EXT] = bundle
 else
-  files.zip(groups_of_search_hits).each_slice(2) do |pair_of_file_and_hit_pairs|
-    (tfile_hits, dfile_hits) = pair_of_file_and_hit_pairs
-    file = tfile_hits.first
-    to_run[file.chomp(File.extname(file)) + EXT] = [tfile_hits.last, dfile_hits.last]
+  files.zip(bundles) do |file, bundle|
+    to_run[file.chomp(File.extname(file)) + EXT] = bundle
   end
 end
 
-to_run.each do |file, target_decoy_pair|
+to_run.each do |file, bundle|
   putsv "calculating qvalues for #{file}"
-  hit_qvalue_pairs = Mspire::ErrorRate::Qvalue.target_decoy_qvalues(target_decoy_pair.first, target_decoy_pair.last, :z_together => opt[:z_together]) {|hit| hit.search_scores[:ionscore] }
+  hit_qvalue_pairs = Mspire::ErrorRate::Qvalue.target_decoy_qvalues(bundle.target, bundle.decoy, :z_together => opt[:z_together]) {|hit| hit.search_scores[:ionscore] }
   hits = [] ; qvals = []
   hit_qvalue_pairs.each do |hit, qval|
     hits << hit ; qvals << qval
