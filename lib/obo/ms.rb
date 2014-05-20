a79b061dfc24c8c91b575bd6d52207cbd3c6229c
John Prince <jtprince@gmail.com>
2014-05-19 23:15:55 -0600

    progress

--- lib/obo/ms.rb
+++
@@ -1,7 +0,0 @@
-require 'obo/ontology'
-
-module Obo
-  MS = Obo::Ontology.new(Obo::Ontology::DIR + '/ms.obo')
-end
-
-
--- spec/mspire/mzml/cv_spec.rb
+++ spec/mspire/mzml/cv_spec.rb
@@ -6,6 +6,12 @@ describe Mspire::Mzml::CV do
   
   it 'can make CVList xml' do
     cvs = [Mspire::Mzml::CV::MS, Mspire::Mzml::CV::UO, Mspire::Mzml::CV::IMS]
+    cvs.each do |cv|
+      [true, false].each do |ext|
+        cv.basename(ext)
+      end
+    end
+ 
     b = Builder::XmlMarkup.new(:indent => 2)
     Mspire::Mzml::CV.list_xml(cvs, b)
     xml = b.to_xml
